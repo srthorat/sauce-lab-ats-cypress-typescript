82f8139bb99fc5f3add00495ce9a4cc80529bf44
Sakharam Thorat <sakharam.thorat@vonage.com>
2024-07-13 10:36:16 +0530

    add assertions to login flow

--- cypress/page-objects/login-page.ts
+++ cypress/page-objects/login-page.ts
@@ -6,13 +6,30 @@ class LoginPage extends BasePage {
   private userNameInput = '#user-name';
   private passwordInput = '#password';
   private loginButton = '#login-button';
+  private errorMessageContainer = '.error-message-container';
 
   // Method to login with credentials
   public login(username: string, password: string) {
     this.type(this.userNameInput, username);
     this.type(this.passwordInput, password);
     this.click(this.loginButton);
-    cy.logAndReport(`==> Successfully logged in with username: ${username}`);
+    cy.logAndReport(`==> Attempted to log in with username: ${username}`);
+    // Check for login error
+    cy.get('body').then(($body) => {
+      if ($body.find(this.errorMessageContainer).length > 0) {
+        cy.get(this.errorMessageContainer)
+          .invoke('text')
+          .then((errorMessage) => {
+            throw new Error(`Login failed with error message: ${errorMessage}`);
+          });
+      } else {
+        // Verify if navigated to inventory page
+        cy.url().should('include', '/inventory.html');
+        cy.logAndReport(
+          `==> Successfully logged in and navigated to inventory page.`,
+        );
+      }
+    });
   }
   public verifyPageTitleDisplayedCorrectly(titleContains: string) {
     cy.title().should('contain', titleContains);
